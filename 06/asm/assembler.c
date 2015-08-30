fd97bd4e736122859a74261284ea89f5e6541980
Matthew-Bird <matthew.james.bird@gmail.com>
2015-08-30 22:58:24 +0100

    file interaction sorted

--- 06/asm/assembler.c
+++ 06/asm/assembler.c
@@ -1,3 +1,12 @@
+/**
+ *	file: assembler.c
+ *	author: matthew.james.bird@gmail.com
+ *
+ *	assembler for the hack assembly language.
+ *
+ *	usage: assembler source output
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +20,31 @@ void itob(int v)
 		putchar('0' + ((v >> i) & 1));
 }
 
-int main(void)
-{	
+int main(int argc, char* argv[])
+{
+	// check input is correct
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: assembler source output\n");
+		return 1;
+	}
+	
+	// open source file
+	FILE* source = fopen(argv[1], "rb");
+	if (source == NULL)
+	{
+		fprintf(stderr, "Error: cannot open source file %s", argv[1]);
+		return 1;
+	}
+	
+	// open output file
+	FILE* out = fopen(argv[2], "wb");
+	if (out == NULL)
+	{
+		fprintf(stderr, "Error: cannot open output file %s", argv[2]);
+		fclose(source);
+		return 1;
+	}
+	
 	
 }
