fe16be623e185b834d889ba4ef11ecfd1222569b
Matthew-Bird <matthew.james.bird@gmail.com>
2015-08-31 17:43:32 +0100

    c-instruction feed working, program passed phase1-test (diff phase1-compare.txt and out.txt)

--- 06/asm/assembler.c
+++ 06/asm/assembler.c
@@ -11,13 +11,97 @@
 #include <stdbool.h>	// bool type
 #include <ctype.h>		// isspace(), isdigit()
 #include <stdlib.h>		// atoi()
-#include <string.h>		// strcpy()
+#include <string.h>		// strcpy(), strcmp(), strchr()
 
 #define MAX_A 32767
+#define COMP_TABLE_SIZE 28
+#define JUMP_TABLE_SIZE 7
+
+// node for comp code and its translation
+typedef struct compNode 
+{
+	char entry[4];
+	char translation[8];
+}
+compNode;
+
+// node for jump code and its translation
+typedef struct jumpNode 
+{
+	char entry[4];
+	char translation[4];
+}
+jumpNode;
+
+// table for comp codes and their translations
+compNode* compDict[COMP_TABLE_SIZE];
+
+// table for jump codes and their translations
+jumpNode* jumpDict[JUMP_TABLE_SIZE];
+
+const char* compCodes[COMP_TABLE_SIZE] = {"0", "1", "-1", "D", "A", "!D", "!A", "-D", "-A",
+									"D+1", "A+1", "D-1", "A-1", "D+A", "D-A", "A-D",
+									"D&A", "D|A", "M", "!M", "-M", "M+1", "M-1", "D+M",
+									"D-M", "M-D", "D&M", "D|M"};
+									
+const char* compTranslations[COMP_TABLE_SIZE] = {"0101010", "0111111", "0111010", "0001100",
+										  "0110000", "0001101", "0110001", "0001111",
+									  	  "0110011", "0011111", "0110111", "0001110",
+									      "0110010", "0000010", "0010011", "0010011",
+									  	  "0000000", "0010101", "1110000", "1110001",
+									  	  "1110011", "1110111", "1110010", "1000010",
+									  	  "1010011", "1000111", "1000000", "1010101"};
+
+const char* jumpCodes[JUMP_TABLE_SIZE] = {"JGT", "JEQ", "JGE", "JLT", "JNE", "JLE", "JMP"};
+
+const char* jumpTranslations[JUMP_TABLE_SIZE] = {"001", "010", "011", "100", "101", 
+										  "110", "111"};
+
+/**
+ *	buildTables: builds the table for comp/jump codes and their translations.
+ */
+void buildTables(void)
+{
+	int i;
+	
+	// build comp table
+	for (i = 0; i < COMP_TABLE_SIZE; i++)
+	{
+		compNode* temp = malloc(sizeof(compNode));
+		strcpy(temp->entry, compCodes[i]);
+		strcpy(temp->translation, compTranslations[i]);
+		compDict[i] = temp;
+	}
+	
+	// build jump table
+	for (i = 0; i < JUMP_TABLE_SIZE; i++)
+	{
+		jumpNode* temp = malloc(sizeof(jumpNode));
+		strcpy(temp->entry, jumpCodes[i]);
+		strcpy(temp->translation, jumpTranslations[i]);
+		jumpDict[i] = temp;
+	}
+}
+
+/**
+ *	clearTables: frees the tables of comp/jump codes and their translations.
+ */
+void clearTables(void)
+{
+	int i;
+	for (i = 0; i < COMP_TABLE_SIZE; i++)
+	{
+		free(compDict[i++]);
+	}
+	for (i = 0; i < JUMP_TABLE_SIZE; i++)
+	{
+		free(jumpDict[i++]);
+	}
+}
 
 /** 
-*	decodeA: reads in an A instruction from source, and outputs the a-instruction to out, converted to binary.
-*	returns source line number
+ *	decodeA: reads in an A instruction from source, and outputs the a-instruction to out, converted to binary.
+ *	returns source line number, or -1 if error.
 */
 int decodeA(FILE* source, FILE* output, int line)
 {
@@ -70,11 +154,54 @@ int decodeA(FILE* source, FILE* output, int line)
 	return line;
 }
 
+/**
+ *	writeComp: translates comp and outputs it to output.
+ *	returns 0 on success, -1 on failure.
+ */
+int writeComp(char* comp, FILE* output)
+{
+	// search computations for the comp
+	int i;
+	for (i = 0; i < COMP_TABLE_SIZE; i++)
+	{
+		if (strcmp(compDict[i]->entry, comp) == 0)
+		{
+			// found
+			fprintf(output, compDict[i]->translation);
+			return 0;
+		}
+	}
+	// not found
+	printf("%s not found\n", comp);
+	return -1;
+}
+
+/**
+ *	writeJump: translates jump and outputs it to output.
+ *	returns 0 on success, -1 on failure.
+ */
+int writeJump(char* jump, FILE* output)
+{
+	// search jump table for the jump
+	int i;
+	for (i = 0; i < JUMP_TABLE_SIZE; i++)
+	{
+		if (strcmp(jumpDict[i]->entry, jump) == 0)
+		{
+			// found
+			fprintf(output, jumpDict[i]->translation);
+			return 0;
+		}
+	}
+	// not found
+	return -1;
+}
+
 /** 
-*	decodeC: reads in a C instruction from source, and outputs the C-instruction to out, converted to binary.
-*	returns line number.
+ *	decodeC: reads in a C instruction from source (first char is c), and outputs the C-instruction to out, converted to binary.
+ *	returns line number, or -1 on error.
 */
-int decodeC(FILE* source, FILE* output, int line)
+int decodeC(char c, FILE* source, FILE* output, int line)
 {
 	// C-instructions have three parts: dest, comp, and jump.
 	char* dest = malloc(4);
@@ -85,13 +212,12 @@ int decodeC(FILE* source, FILE* output, int line)
 		fprintf(stderr, "Error (line: %d): cannot malloc\n", line);
 	}
 	
-	char c;
 	char* buffer = malloc(4);
 	int i = 0;
-	bool dest = false;
-	bool comp = false;
-	bool jump = false;
-	while ((c = fgetc(source)) != EOF)
+	bool destIn = false;
+	bool compIn = false;
+	bool jumpIn = false;
+	do
 	{
 		if (i > 3)
 		{
@@ -99,44 +225,108 @@ int decodeC(FILE* source, FILE* output, int line)
 		}
 		else if (c == '=')	// buffer is dest
 		{
-			// strcpy buffer to dest
-			// dest = true
-			// set i = 0
+			strcpy(dest, buffer);
+			dest[i] = '\0';
+			destIn = true;
+			i = 0;
 		}
-		else if ((c == '\n' && !comp) || c == ';')	// buffer is comp
+		else if ((((c == '\n') || (c == '/') || c == EOF) && !compIn) || c == ';')	// buffer is comp
 		{
-			// strcpy buffer to dest
-			// comp = true
-			// set i = 0
+			strcpy(comp, buffer);
+			comp[i] = '\0';
+			compIn = true;
+			i = 0;
 		}
-		else if (c == '\n' && comp)	// buffer is jump
+		else if (((c == '\n') || (c == '/') || c == EOF) && compIn)	// buffer is jump
 		{
-			// strcpy buffer to jump
-			// jump = true
-			// set i = 0
+			strcpy(jump, buffer);
+			jump[i] = '\0';
+			jumpIn = true;
+			i = 0;
 		}
-		if (c == '\n')
+		else if (!isspace(c) && c != '/')
 		{
-			line++;
+			buffer[i++] = c;
+		}
+		if (c == '\n' || c == '/' || c == EOF)
+		{
 			break;
 		}
-		if (c == '/')
-		{
-			break;
-		}
-		// ...
-		// buffer[i++] = c;
 	}
+	while (c = fgetc(source));
 	
 	// write C-instruction code (111)
-	// write comp (if comp and not in table, error)
-	// write dest
-	// write jump
+	fprintf(output, "111");
+	
+	if (compIn) 
+	{
+		if (writeComp(comp, output) == -1)
+		{
+			fprintf(stderr, "Error (line: %d): cannot translate '%s'\n", line, comp);
+		}
+	}
+	else
+	{
+		// write default comp code
+		fprintf(output, "111101010");
+	}
+	
+	if (destIn)
+	{
+		if (strchr(dest, 'A') != NULL)
+		{
+			fputc('1', output);
+		}
+		else
+		{
+			fputc('0', output);
+		}
+		if (strchr(dest, 'D') != NULL)
+		{
+			fputc('1', output);
+		}
+		else
+		{
+			fputc('0', output);
+		}
+		if (strchr(dest, 'M') != NULL)
+		{
+			fputc('1', output);
+		}
+		else
+		{
+			fputc('0', output);
+		}
+	}
+	else
+	{
+		// write default dest
+		fprintf(output, "000");
+	}
+	
+	if (jumpIn)
+	{
+		if (writeJump(jump, output) == -1)
+		{
+			fprintf(stderr, "Error (line: %d): cannot translate jump '%s'\n", line, jump);
+		}
+	}
+	else
+	{
+		// write default jump
+		fprintf(output, "000");
+	}
 	
 	free(dest);
 	free(comp);
 	free(jump);
+	free(buffer);
+	
+	fputc('\n', output);
+	line++;
+	return line;
 }
+
 
 int main(int argc, char* argv[])
 {
@@ -163,6 +353,9 @@ int main(int argc, char* argv[])
 		fclose(source);
 		return 1;
 	}
+	
+	// build translation tables
+	buildTables();
 	
 	// main read loop
 	char c;
@@ -194,7 +387,19 @@ int main(int argc, char* argv[])
 		}
 		else 				// C-INSTRUCTION (or invalid)
 		{
-			line = decodeC(source, output, line);
+			line = decodeC(c, source, output, line);
+		}
+		if (line == -1)
+		{
+			return 1;
 		}
 	}
+	
+	// clear translation tables
+	clearTables();
+	
+	fclose(source);
+	fclose(output);
+	
+	printf("Assembly successful\n");
 }
