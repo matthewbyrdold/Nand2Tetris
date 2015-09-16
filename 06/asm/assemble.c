90ccfecf65e810e76c9da7e709bcac8cf19004e8
Matthew Byrd <matthew.james.byrd@gmail.com>
2015-09-16 10:51:44 +0000

    Update documentation

--- 06/asm/assemble.c
+++ 06/asm/assemble.c
@@ -2,9 +2,8 @@
  *	file: assemble.c
  *	author: matthew.james.bird@gmail.com
  *
- *	assembler for the hack assembly language.
+ *	assembler functions for the hack assembly language.
  *
- *	usage: assembler source output
  */
 
 #include "assemble.h"
@@ -573,7 +572,9 @@ bool loadLabels(FILE* source)
 	return true;
 }
 
-
+/**
+ * assemble: main assembly function. Converts instructions in source to machine code.
+ */
 bool assemble(FILE* source, FILE* output)
 {
 	// build translation tables
