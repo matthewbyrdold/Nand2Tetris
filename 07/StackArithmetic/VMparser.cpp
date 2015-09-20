fe6f63652908c80a6d1626b7ddd96c079e3797fb
Matthew-Bird <matthew.james.bird@gmail.com>
2015-09-20 18:29:12 +0100

    rename files

---
+++ 07/StackArithmetic/VMparser.cpp
@@ -0,0 +1,149 @@
+/**
+ *	file: VMparser.cpp
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "VMparser.hpp"
+
+using namespace std;
+
+// -------------------------------
+// Parser member functions
+// -------------------------------
+
+Parser::Parser(ifstream& file)
+	: source(file)
+{}
+	
+/** Returns whether there are more commands in source. */
+bool Parser::hasMoreCommands()
+{
+	return (source.peek() != EOF);
+}
+
+/** Reads the next command from input. */
+void Parser::advance()
+{
+	string temp;
+	char c;
+	bool insertSpace = false;
+	while ((source >> noskipws >> c) && (c != '\n') && (c != '/'))
+	{
+		if (isspace(c) && insertSpace)
+		{
+			temp += c;
+			insertSpace = false;
+		}
+		else if (!isspace(c))
+		{
+			temp += c;
+			insertSpace = true;
+		}
+	}
+	
+	// continue reading until newline or EOF
+	while (c != '\n' && c != EOF && (source >> noskipws >> c));
+	
+	command = temp;
+	
+	// TEST CODE
+	if (command.size() > 1)// debug
+	{
+		cout << command << endl;	// debug
+		if (commandType() != C_RETURN)
+		{
+			cout << "First arg: [" << arg1() << "]" << endl;//debug
+		}
+		if (commandType() == C_PUSH || commandType() == C_POP || commandType() == C_FUNCTION
+				|| commandType() == C_CALL)
+		{
+			cout << "Second arg: [" << arg2() << "]" << endl;
+		}
+		//debugEND
+	}//debugEND
+}
+
+/** Returns the type of the current command. */
+Parser::command_t Parser::commandType()
+{
+	// TODO
+	if (command.compare(0, 4, "push") == 0)
+	{
+		return C_PUSH;
+	}
+	else if (command.compare(0, 3, "pop") == 0)
+	{
+		return C_POP;
+	}
+	else if (command.compare(0, 5, "label") == 0)
+	{
+		return C_LABEL;
+	}
+	else if (command.compare(0, 4, "goto") == 0)
+	{
+		return C_GOTO;
+	}
+	else if (command.compare(0, 2, "if") == 0)
+	{
+		return C_IF;
+	}
+	else if (command.compare(0, 8, "function") == 0)
+	{
+		return C_FUNCTION;
+	}
+	else if (command.compare(0, 6, "return") == 0)
+	{
+		return C_RETURN;
+	}
+	else if (command.compare(0, 4, "call") == 0)
+	{
+		return C_CALL;		// TODO: is this the correct format for calls?
+	}
+	else 
+	{
+		return C_ARITHMETIC;
+	}
+}
+
+/** Returns the first argument of the current command.
+	In the case of C_ARITHMETIC, the command itself (add, sub, etc.) is returned.
+	Should not be called in the current command is C_RETURN */
+string Parser::arg1()
+{
+	if (commandType() == C_ARITHMETIC)
+	{
+		return command;
+	}
+	else
+	{
+		return nth_argument(1);
+	}
+}
+
+/** Returns the second argument of the current command.
+	Should be called only if the current command is C_PUSH, C_POP, C_FUNCTION, or C_CALL. */
+string Parser::arg2()
+{
+	return nth_argument(2);
+}
+
+/** Returns the nth argument of the current command. */
+string Parser::nth_argument(int n)
+{
+	string buf;
+	stringstream ssCommand(command);
+	int word = 0;
+	while (word++ != n+1)
+	{
+		ssCommand >> buf;
+	}
+	return buf;
+}
+
+
+
---
+++ 07/StackArithmetic/VMparser.hpp
@@ -0,0 +1,61 @@
+/**
+ *	file: VMparser.hpp
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+/**
+ *	Class: Parser
+ *	Handles the parsing of a single .vm file, and encapsulates access to the input code. 
+ *	It reads VM commands, parses them, and provides convenient access to their components. 
+ *	In addition, it removes all white space and comments.
+ */
+class Parser
+{
+public:
+	Parser(ifstream&);
+
+	/**
+	 * 	Returns whether there are more commands in source. 
+	 */
+	bool hasMoreCommands();
+	
+	/** 
+	 *	Reads the next command from input.
+	 *	Should not be called if hasMoreCommands() is false. 
+	 */
+	void advance();
+	
+	enum command_t {C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL};
+	
+	/**
+	 *	Returns the type of the current command. 
+	 */
+	command_t commandType();
+	
+	/** 
+	 *	Returns the first argument of the current command.
+	 *	In the case of C_ARITHMETIC, the command itself (add, sub, etc.) is returned.
+	 *	Should not be called in the current command is C_RETURN.
+	 */
+	string arg1();	
+	
+	/** 
+	 *	Returns the second argument of the current command.
+	 *	Should be called only if the current command is C_PUSH, C_POP, C_FUNCTION, or C_CALL. 
+	 */
+	string arg2();
+		
+private:
+	/** Returns the nth argument of the current command. */
+	string nth_argument(int);
+		
+	ifstream& source;
+	string command;
+};
