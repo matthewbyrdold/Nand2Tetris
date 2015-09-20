eb55e8e8a30d8d5478482294fe57b430b07b700c
Matthew-Bird <matthew.james.bird@gmail.com>
2015-09-20 14:26:06 +0100

    small changes to parser member function declarations

--- 07/StackArithmetic/vmtrans.cpp
+++ 07/StackArithmetic/vmtrans.cpp
@@ -21,17 +21,17 @@ bool Parser::hasMoreCommands()
 	return (source.peek() != EOF);
 }
 
-/** Reads the next command from input.
-	Should not be called if hasMoreCommands() is false. */
-void advance()
+/** Reads the next command from input. */
+void Parser::advance()
 {
-	// TODO
+	if (hasMoreCommands())
+	{
+		getline(source, command);
+	}
 }
 
-enum command_t {C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL};
-
 /** Returns the type of the current command. */
-command_t CommandType()
+Parser::command_t Parser::CommandType()
 {
 	// TODO
 	return C_ARITHMETIC;
@@ -40,7 +40,7 @@ command_t CommandType()
 /** Returns the first argument of the current command.
 	In the case of C_ARITHMETIC, the command itself (add, sub, etc.) is returned.
 	Should not be called in the current command is C_RETURN */
-string arg1()
+string Parser::arg1()
 {
 	// TODO
 	return "test";
@@ -48,7 +48,7 @@ string arg1()
 
 /** Returns the second argument of the current command.
 	Should be called only if the current command is C_PUSH, C_POP, C_FUNCTION, or C_CALL. */
-string arg2()
+string Parser::arg2()
 {
 	// TODO
 	return "test2";
