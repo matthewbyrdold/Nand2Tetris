a13ae80933d8b5db2a6a2ab250dc19cda81af802
Matthew-Bird <matthew.james.bird@gmail.com>
2015-09-20 22:18:45 +0100

    started assembly translation (add and sub done)

--- 07/StackArithmetic/CodeWriter.cpp
+++ 07/StackArithmetic/CodeWriter.cpp
@@ -24,13 +24,27 @@ CodeWriter::~CodeWriter()
 /**	  Informs the code writer that the translation of a new VM file has started.  */
 void CodeWriter::setFileName(string file)
 {
-	filename = file;
+	fileName = file;
 }
 
 /**   Writes the assembly code that is the translation of the given arithmetic command.  */
-void CodeWriter::writeArithmetic(string)
+void CodeWriter::writeArithmetic(string command)
 {
-	//TODO
+	//TODO finish options
+	if (command == "add")
+	{
+		popD();
+		decSP();
+		output << "M = D+M" << endl;
+		incSP();
+	}
+	else if (command == "sub")
+	{
+		popD();
+		decSP();
+		output << "M = M-D" << endl;
+		incSP();
+	}
 }
 
 /**   Writes the assembly code that is the translation of the given command (either C_PUSH or C_POP)  */
@@ -38,3 +52,42 @@ void writePushPop(CodeWriter::command_t command, string segment, int index)
 {
 	// TODO
 }
+
+
+/* -------------------------------------------------------------------------------------------
+ *	Assembly-translation helper methods
+ *		Each of the below functions writes an often-used piece of VM-logic to the output in assembly code.
+ * ------------------------------------------------------------------------------------------- */
+
+/**
+*	Writes the assembly code for popping the stack to the D register.
+*/
+void CodeWriter::popD()
+{
+	output << "@SP" << endl;
+	output << "AM = M-1" << endl;
+	output << "D = M" << endl;
+}
+
+/**
+*	Writes the assembly code for decrementing the stack pointer.
+*/
+void CodeWriter::decSP()
+{
+	output << "@SP" << endl;
+	output << "AM = M-1" << endl;
+}
+
+/**
+*	Writes the assembly code for incrementing the stack pointer.
+*/
+void CodeWriter::incSP()
+{
+	output << "@SP" << endl;
+	output << "AM = M+1" << endl;
+}
+
+
+
+
+
--- 07/StackArithmetic/CodeWriter.hpp
+++ 07/StackArithmetic/CodeWriter.hpp
@@ -31,8 +31,32 @@ public:
     */
 	void writePushPop(command_t, string, int);
 	
-	
 private:
 	ofstream& output;
-	string filename;
+	string fileName;
+	
+   /* -------------------------------------------------------------------------------------------
+    *	Assembly-translation helper methods
+    *		Each of the below functions writes an often-used piece of VM-logic to the output in assembly code.
+    * ------------------------------------------------------------------------------------------- */
+	
+   	/**
+    *	Writes the assembly code for popping the stack to the D register.
+    */
+	void popD();
+	
+   	/**
+    *	Writes the assembly code for decrementing the stack pointer.
+    */
+	void decSP();
+	
+   	/**
+    *	Writes the assembly code for incrementing the stack pointer.
+    */
+	void incSP();
 };
+
+
+
+
+
