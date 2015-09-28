c6aeaf355261e446c88a4c1356b982fe97d8de64
Matthew Byrd <matthew.james.byrd@gmail.com>
2015-09-28 15:43:45 +0100

    skeleton of directory reader

--- 07/VMT/VMtranslator.cpp
+++ 07/VMT/VMtranslator.cpp
@@ -12,23 +12,19 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <dirent.h>
 
 #include "Parser.hpp"
 #include "CodeWriter.hpp"
 #include "VMT.hpp"
 
 using namespace std;
+using namespace vmt;
 
-
-/*  OPEN MANY FILES:
-
-#include <dirent.h>
-
-vector<string> open(string path = ".") {
-
-    DIR*    dir;
+vector<string> getFiles(string path = ".") {
+    DIR* dir;
     dirent* pdir;
-    std::vector<std::string> files;
+    vector<string> files;
 
     dir = opendir(path.c_str());
 
@@ -40,79 +36,82 @@ vector<string> open(string path = ".") {
 }
 
 
-IN LOOP:
-
-    vector<string> f;
-
-    f = open(); // or pass which dir to open
-
-    fstream file;
-
-    file.open(f[0]);
-
-    return 0;
-
-*/
-
-
-using namespace std;
-using namespace vmt;
-
 int main(int argc, char* argv[])
 {
+	
 	if (argc != 2)
 	{
 		cerr << "Usage: ./VMtranslator source" << endl;
 		return 1;
 	}
 	
-	// TODO: DIRECTORY INPUT!
-	// if argv[1][end] != .vm, then go through named directory file by file, doing a different parser for each
-	// eventual usuage should be fully abstracted: for each file, create a vm object and call vm.translate("filename")
-	
-	ifstream source;
-	source.open(argv[1]);
-	if (!source.is_open())
+	////////////////////////////////////////
+	if (argv[1].substr(argv[1].size()-3, arg[1].size()-1) == ".vm")  // single .vm file
 	{
-		cerr << "Cannot open source file " << argv[1] << endl;
-		return 1;
-	}
-	
-	ofstream output;
-	output.open("test.asm");
-	if (!output.is_open())
-	{
-		cerr << "Cannot open source file test.asm" << endl;
-		return 1;
-	}
+		ifstream source;
+		source.open(argv[1]);
+		if (!source.is_open())
+		{
+			cerr << "Cannot open source file " << argv[1] << endl;
+			return 1;
+		}
 		
-	// Parse the file
-	Parser parser = Parser(source);
-	CodeWriter writer = CodeWriter(output);
-	
-	// TEST CODE
-	while (parser.hasMoreCommands())
-	{
-		if (parser.advance() == false)
+		ofstream output;
+		output.open("test.asm");
+		if (!output.is_open())
 		{
-			continue;
+			cerr << "Cannot open source file test.asm" << endl;
+			return 1;
 		}
-		if (parser.commandType() == C_PUSH)
+			
+		// Parse the file
+		Parser parser = Parser(source);
+		CodeWriter writer = CodeWriter(output);
+		
+		// TEST CODE
+		while (parser.hasMoreCommands())
 		{
-			int i = atoi(parser.arg2().c_str());
-			writer.writePushPop(C_PUSH, parser.arg1(), i);
+			if (parser.advance() == false)
+			{
+				continue;
+			}
+			if (parser.commandType() == C_PUSH)
+			{
+				int i = atoi(parser.arg2().c_str());
+				writer.writePushPop(C_PUSH, parser.arg1(), i);
+			}
+			else if (parser.commandType() == C_POP)
+			{
+				int i = atoi(parser.arg2().c_str());
+				writer.writePushPop(C_POP, parser.arg1(), i);
+			}
+			else if (parser.commandType() == C_ARITHMETIC)
+			{
+				writer.writeArithmetic(parser.arg1());
+			}
 		}
-		else if (parser.commandType() == C_POP)
-		{
-			int i = atoi(parser.arg2().c_str());
-			writer.writePushPop(C_POP, parser.arg1(), i);
-		}
-		else if (parser.commandType() == C_ARITHMETIC)
-		{
-			writer.writeArithmetic(parser.arg1());
-		}
+		
+		source.close();
+		output.close();
 	}
+	else         // directory
+	{
+		while (STUFF IN VECTOR)
+		{
+			vector<string> f;
+			
+			f = getFiles(argv[1]); // or pass which dir to open
+			
+			fstream file;
+			
+			file.open(f[0]);
+			if (!file.is_open())
+			{
+				cerr << "Error: cannot open " << f[n] << endl;
+	    			return 1;
+			}
 	
-	source.close();
-	output.close();
+			 return 0;
+		}
+	}	
 }
