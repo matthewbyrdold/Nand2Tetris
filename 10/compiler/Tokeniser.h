882ec1134249700b8b027f8f48a4861a13be3bd4
Matthew-Bird <matthew.james.bird@gmail.com>
2016-08-24 22:20:52 +0100

    Initial commit of rough overview of classes

---
+++ 10/compiler/Tokeniser.h
@@ -0,0 +1,18 @@
+/**   Tokeniser.h
+ *    Splits Jack files into tokens.
+ *    matthew.james.bird@gmail.com
+ */
+
+#ifndef tokeniser_h
+#define tokeniser_h
+
+/** 
+ * Removes all comments and whitespace from the input stream and breaks it into
+ * Jack-language tokens, as specified by the Jack-grammar.
+ */
+class Tokeniser {
+public:
+
+};
+
+#endif // tokeniser_h    
